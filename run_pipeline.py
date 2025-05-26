eb9a0354e4bb04e339241cd6498b63d9361eb5a8
NumanBashir <83068021+NumanBashir@users.noreply.github.com>
2025-05-26 15:16:58 +0200

    clean up

--- run_pipeline.py
+++ run_pipeline.py
@@ -10,21 +10,22 @@ import pandas as pd
 #     pd.read_csv("input_data/gca1.csv"),  # Example of manual file 
 # ]
 
+# Remember to change URL and table_id
 tables = [
     scrape_table("https://fbref.com/en/comps/10/possession/Championship-Stats", "stats_possession"),
     scrape_table("https://fbref.com/en/comps/10/gca/Championship-Stats", "stats_gca"),
 ]
 
 # Step 2: Merge
-merge_data(tables).to_csv("input_data/championship_merged.csv", index=False)
-merged_df = pd.read_csv("input_data/championship_merged.csv")
+merge_data(tables).to_csv("output\Championship_24_25\merged.csv", index=False)
+merged_df = pd.read_csv("output\Championship_24_25\merged.csv")
 
 # Step 3: Per 90
 per90_df = calculate_per90(merged_df)
-per90_df.to_csv("input_data/championship_per90.csv", index=False)
+per90_df.to_csv("output\Championship_24_25\per90.csv", index=False)
 
 # Step 4: Filter by 90s played
 filtered_df = filter_by_90s(per90_df, min_90s=4.0)
-filtered_df.to_csv("input_data/championship_filtered.csv", index=False)
+filtered_df.to_csv("output\Championship_24_25\filter.csv", index=False)
 
 print("Pipeline complete ✅")
