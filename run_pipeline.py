c2e93fb7a0d7f88dbf0b227a090aeb24d86c7a40
NumanBashir <83068021+NumanBashir@users.noreply.github.com>
2025-05-30 10:14:02 +0200

    bundesliga 24_25

--- run_pipeline.py
+++ run_pipeline.py
@@ -12,30 +12,30 @@ import pandas as pd
 
 # Remember to change URL and table_id
 tables = [
-    scrape_table("https://fbref.com/en/comps/37/stats/Belgian-Pro-League-Stats", "stats_standard"),
-    scrape_table("https://fbref.com/en/comps/37/shooting/Belgian-Pro-League-Stats", "stats_shooting"),
-    scrape_table("https://fbref.com/en/comps/37/passing/Belgian-Pro-League-Stats", "stats_passing"),
-    scrape_table("https://fbref.com/en/comps/37/passing_types/Belgian-Pro-League-Stats", "stats_passing_types"),
-    scrape_table("https://fbref.com/en/comps/37/gca/Belgian-Pro-League-Stats", "stats_gca"),
-    scrape_table("https://fbref.com/en/comps/37/defense/Belgian-Pro-League-Stats", "stats_defense"),
-    scrape_table("https://fbref.com/en/comps/37/possession/Belgian-Pro-League-Stats", "stats_possession"),
-    scrape_table("https://fbref.com/en/comps/37/playingtime/Belgian-Pro-League-Stats", "stats_playing_time"),
-    scrape_table("https://fbref.com/en/comps/37/misc/Belgian-Pro-League-Stats", "stats_misc"),
+    scrape_table("https://fbref.com/en/comps/20/stats/Bundesliga-Stats", "stats_standard"),
+    scrape_table("https://fbref.com/en/comps/20/shooting/Bundesliga-Stats", "stats_shooting"),
+    scrape_table("https://fbref.com/en/comps/20/passing/Bundesliga-Stats", "stats_passing"),
+    scrape_table("https://fbref.com/en/comps/20/passing_types/Bundesliga-Stats", "stats_passing_types"),
+    scrape_table("https://fbref.com/en/comps/20/gca/Bundesliga-Stats", "stats_gca"),
+    scrape_table("https://fbref.com/en/comps/20/defense/Bundesliga-Stats", "stats_defense"),
+    scrape_table("https://fbref.com/en/comps/20/possession/Bundesliga-Stats", "stats_possession"),
+    scrape_table("https://fbref.com/en/comps/20/playingtime/Bundesliga-Stats", "stats_playing_time"),
+    scrape_table("https://fbref.com/en/comps/20/misc/Bundesliga-Stats", "stats_misc"),
 ]
 
 # Step 2: Merge
-merge_data(tables).to_csv("output/JupilerProLeague_24_25/merged_jpl_24_25.csv", index=False)
-df = pd.read_csv("output/JupilerProLeague_24_25/merged_jpl_24_25.csv")
+merge_data(tables).to_csv("output/Bundesliga_24_25/merged_bundesliga_24_25.csv", index=False)
+df = pd.read_csv("output/Bundesliga_24_25/merged_bundesliga_24_25.csv")
 
 # Step 3: Per 90
 df = calculate_per90(df)
-df.to_csv("output/JupilerProLeague_24_25/per90_jpl_24_25.csv", index=False)
+df.to_csv("output/Bundesliga_24_25/per90_bundesliga_24_25.csv", index=False)
 
 # Step 4: Filter by 90s played
 df = filter_by_90s(df, min_90s=5.0)
-df.to_csv("output/JupilerProLeague_24_25/filter_jpl_24_25.csv", index=False)
+df.to_csv("output/Bundesliga_24_25/filter_bundesliga_24_25.csv", index=False)
 
 # Step 5: Reformat for better interpret for PowerBI 
-reformat_powerbi(df, "output/JupilerProLeague_24_25/reformat_jpl_24_25.csv")
+reformat_powerbi(df, "output/Bundesliga_24_25/reformat_bundesliga_24_25.csv")
 
 print("Pipeline complete ✅")
