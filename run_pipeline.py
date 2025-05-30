9a3c666ebbe703de58319aa6188cf2549254e999
NumanBashir <83068021+NumanBashir@users.noreply.github.com>
2025-05-30 08:01:18 +0200

    premier league 24_25

--- run_pipeline.py
+++ run_pipeline.py
@@ -12,23 +12,30 @@ import pandas as pd
 
 # Remember to change URL and table_id
 tables = [
-    scrape_table("https://fbref.com/en/comps/10/possession/Championship-Stats", "stats_possession"),
-    scrape_table("https://fbref.com/en/comps/10/gca/Championship-Stats", "stats_gca"),
+    scrape_table("https://fbref.com/en/comps/9/stats/Premier-League-Stats", "stats_standard"),
+    scrape_table("https://fbref.com/en/comps/9/shooting/Premier-League-Stats", "stats_shooting"),
+    scrape_table("https://fbref.com/en/comps/9/passing/Premier-League-Stats", "stats_passing"),
+    scrape_table("https://fbref.com/en/comps/9/passing_types/Premier-League-Stats", "stats_passing_types"),
+    scrape_table("https://fbref.com/en/comps/9/gca/Premier-League-Stats", "stats_gca"),
+    scrape_table("https://fbref.com/en/comps/9/defense/Premier-League-Stats", "stats_defense"),
+    scrape_table("https://fbref.com/en/comps/9/possession/Premier-League-Stats", "stats_possession"),
+    scrape_table("https://fbref.com/en/comps/9/playingtime/Premier-League-Stats", "stats_playing_time"),
+    scrape_table("https://fbref.com/en/comps/9/misc/Premier-League-Stats", "stats_misc"),
 ]
 
 # Step 2: Merge
-merge_data(tables).to_csv("output\Championship_24_25\merged.csv", index=False)
-df = pd.read_csv("output\Championship_24_25\merged.csv")
+merge_data(tables).to_csv("output/PremierLeague_24_25/merged_premier_league_24_25.csv", index=False)
+df = pd.read_csv("output/PremierLeague_24_25/merged_premier_league_24_25.csv")
 
 # Step 3: Per 90
 df = calculate_per90(df)
-df.to_csv("output\Championship_24_25\per90.csv", index=False)
+df.to_csv("output/PremierLeague_24_25/per90_premier_league_24_25.csv", index=False)
 
 # Step 4: Filter by 90s played
 df = filter_by_90s(df, min_90s=5.0)
-df.to_csv("output\Championship_24_25\\filter.csv", index=False)
+df.to_csv("output/PremierLeague_24_25/filter_premier_league_24_25.csv", index=False)
 
 # Step 5: Reformat for better interpret for PowerBI 
-reformat_powerbi(df, "output/Championship_24_25/final_powerbi.csv")
+reformat_powerbi(df, "output/PremierLeague_24_25/reformat_premier_league_24_25.csv")
 
 print("Pipeline complete ✅")
